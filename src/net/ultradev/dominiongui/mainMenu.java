8341c39bb17d9a0ba4012002b50e8668d9941f1d
BuildTools <unconfigured@null.spigotmc.org>
2016-03-21 19:12:58 +0100

    Basis main menu
    Heb snel eens de basis gemaakt voor de main menu

---
+++ src/net/ultradev/dominiongui/mainMenu.java
@@ -0,0 +1,65 @@
+package net.ultradev.dominiongui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class mainMenu extends Application{
+	@Override
+	public void start(Stage primaryStage){
+		primaryStage.setTitle("Dominion");	
+		
+		BorderPane root = new BorderPane();
+		
+		
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25,25,25,25));
+		
+		Button playLocalBtn = new Button();
+		playLocalBtn.setText("Play local");
+		playLocalBtn.setPrefWidth(100);
+		grid.add(playLocalBtn, 0, 0);
+		
+		Button playOnlineBtn = new Button();
+		playOnlineBtn.setText("Play Online");
+		playOnlineBtn.setPrefWidth(100);
+		grid.add(playOnlineBtn, 0, 1);
+		
+		Button optionsBtn = new Button();
+		optionsBtn.setText("Options");
+		optionsBtn.setPrefWidth(100);
+		grid.add(optionsBtn, 0, 2);
+		
+		root.setCenter(grid);
+				
+		Button exitBtn = new Button();
+		exitBtn.setPrefWidth(100);
+		exitBtn.setText("Exit");
+		
+		HBox bottomBox = new HBox();
+		bottomBox.getChildren().add(exitBtn);
+		bottomBox.setAlignment(Pos.BOTTOM_RIGHT);
+		bottomBox.setPadding(new Insets(10,10,10,10));
+		root.setBottom(bottomBox);
+		
+		
+				
+		Scene scene = new Scene(root,600,500);
+		root.prefHeight(scene.getHeight());
+		root.prefWidth(scene.getWidth());
+		primaryStage.setScene(scene);
+		scene.getStylesheets().add(DominionMainGUI.class.getResource("GUIStyle.css").toExternalForm());
+		
+		primaryStage.show();	
+	}
+}
